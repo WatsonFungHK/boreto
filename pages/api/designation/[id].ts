2e17dd74728431741082c89c1b1287677857483a
charlietanly <62069135+charlietanly@users.noreply.github.com>
2023-06-21 00:53:26 +0800

    feat:api set up

---
+++ pages/api/designation/[id].ts
@@ -0,0 +1,83 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "lib/prisma";
+import cuid from "cuid";
+import { getSession } from "next-auth/react";
+import { companyId } from "pages/api/constants";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method === "GET") {
+      const response = await prisma.designation.findUnique({
+        where: {
+          id: req.query.id as string,
+        },
+        include: {
+          Benefit: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          Department: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+      res.status(200).json(response);
+    }
+    if (req.method === "POST") {
+      const session = await getSession();
+      const { updated_at, created_at, benefit, department, id, ...data } =
+        req.body;
+
+      if (!id) {
+        const createdDesignation = await prisma.designation.create({
+          data: {
+            ...data,
+            companyId,
+            Benefit: {
+              connect: benefit.map((BenefitId) => ({
+                Benefit: { connect: { id: BenefitId } },
+              })),
+            },
+            Department: {
+              connect: department.map((departmentId) => ({
+                department: { connect: { id: departmentId } },
+              })),
+            },
+          },
+        });
+        res.status(200).json(createdDesignation);
+      } else {
+        const updatedDesignation = await prisma.designation.update({
+          where: { id },
+          data: {
+            ...data,
+            Benefit: {
+              connect: benefit.map((BenefitId) => ({
+                Benefit: { connect: { id: BenefitId } },
+              })),
+            },
+            Department: {
+              connect: department.map((departmentId) => ({
+                department: { connect: { id: departmentId } },
+              })),
+            },
+          },
+        });
+        res.status(200).json(updatedDesignation);
+      }
+    }
+  } catch (error) {
+    console.log("error: ", error);
+    res.status(500).json({ error: error.message });
+  }
+}
